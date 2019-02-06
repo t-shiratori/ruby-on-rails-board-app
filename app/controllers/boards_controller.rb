2e4813c59cb09e4e0427158a73b9f481e9442f8b
白鳥貴士 <tks.srtr89@gmail.com>
2019-02-06 22:28:48 +0900

    update

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,35 @@
 class BoardsController < ApplicationController
     def index
+        @boards = Board.all
     end
 
     def new
         @board = Board.new
+    end
+
+    def create
+        Board.create(board_params)
+        # binding.pry
+    end
+
+    def show
+        @board = Board.find(params[:id])
+    end
+
+    def edit
+        @board = Board.find(params[:id])
+    end
+
+    def update
+        board = Board.find(params[:id])
+        board.update(board_params)
+
+        redirect_to board
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:name, :title, :body)
     end
 
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %M月 %d日  %H時 %M分'

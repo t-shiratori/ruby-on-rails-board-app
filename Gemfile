23752c69af3c6ed816cb952d74b8469abfd92532
白鳥貴士 <tks.srtr89@gmail.com>
2019-02-11 20:53:32 +0900

    update

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap', '~> 4.0.0'
 gem 'kaminari'
+gem 'rails-i18n'
 gem 'mini_racer'
 
 group :development, :test do
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
     validates :name, presence: true, length: { maximum: 10 }
     validates :title, presence: true, length: { maximum: 30 }
-    validates :sbody, presence: true, length: { maximum: 1000 }
-end
+    validates :body, presence: true, length: { maximum: 1000 }
+  end

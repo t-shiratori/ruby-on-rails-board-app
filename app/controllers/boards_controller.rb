187d3871931504e86b66c14a6bc08dbe202c4ade
白鳥貴士 <tks.srtr89@gmail.com>
2019-02-21 23:57:29 +0900

    update

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,53 +1,58 @@
 class BoardsController < ApplicationController
-
     before_action :set_target_board, only: %i[show edit update destroy]
-
+  
     def index
-        @boards = Board.page(params[:page])
+      @boards = Board.page(params[:page])
     end
-
+  
     def new
-        @board = Board.new(flash[:board])
+      @board = Board.new(flash[:board])
     end
-
+  
     def create
-        board = Board.new(board_params)
-        if board.save
-            flash[:notice] = "「#{board.title}」の掲示板を作成しました"
-            redirect_to board # 新規作成されたIDに対応するURLにリダイレクトさせる
-        else
-            redirect_to new_board_path, flash: {
-                board: board,
-                error_messages: board.errors.full_messages
-            }
-        end
-        # binding.pry
+      board = Board.new(board_params)
+      if board.save
+        flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+        redirect_to board
+      else
+        redirect_to :back, flash: {
+          board: board,
+          error_messages: board.errors.full_messages
+        }
+      end
     end
-
+  
     def show
-        @comment = Comment.new(board_id: @board.id)
+      @comment = Comment.new(board_id: @board.id)
     end
-
+  
     def edit
+      @board.attributes = flash[:board] if flash[:board]
     end
-
+  
     def update
-        @board.update(board_params)
+      if @board.update(board_params)
         redirect_to @board
+      else
+        redirect_to :back, flash: {
+          board: @board,
+          error_messages: @board.errors.full_messages
+        }
+      end
     end
-
+  
     def destroy
-        @board.delete
-        redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました"}
+      @board.delete
+      redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
     end
-
+  
     private
+  
     def board_params
-        params.require(:board).permit(:name, :title, :body)
+      params.require(:board).permit(:name, :title, :body)
     end
-
+  
     def set_target_board
-        @board = Board.find(params[:id])
+      @board = Board.find(params[:id])
     end
-
-end
+  end

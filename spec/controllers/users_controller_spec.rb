9b07c2c8ab4ec6e9c5b2d004a009bd6bb273385c
白鳥貴士 <tks.srtr89@gmail.com>
2019-05-04 18:24:30 +0900

    コントローラーのnewメソッドのテストを追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+    describe 'Get #new' do
+        # このコントローラーのnewアクションにgetのhttpメソッドでアクセスすることを擬似的に行なっている
+        # getのリクエスト結果がresponse変数に格納される
+        before { get :new }
+        it 'レスポンスコードが200であること' do
+            expect(response).to have_http_status(:ok)
+        end
+
+        it 'newテンプレートをレンダリングすること' do
+            expect(response).to render_template :new
+        end
+
+        it '新しいuserオブジェクトがビューに渡されること' do
+            expect(assigns(:user)).to be_a_new User
+        end
+
+    end
 
 end
